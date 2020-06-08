0147c132b21a37ed6c746ce96929162d30c3b52a
loadev2 <loadev@gmail.com>
2020-06-08 13:12:25 +0300

    add data for heroku

--- pro_d3/settings.py
+++ pro_d3/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^wau&98q8-7bdrxsnfzh0_$z5^c7t5cyjd9ny7eoa4)oxvpiu-'
-
+# SECRET_KEY = '^wau&98q8-7bdrxsnfzh0_$z5^c7t5cyjd9ny7eoa4)oxvpiu-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
